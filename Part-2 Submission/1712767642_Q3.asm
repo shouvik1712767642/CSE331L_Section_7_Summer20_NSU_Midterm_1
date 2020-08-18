fc02aaa5572d017ff4daf2ca46083f08a9a011d6
S.M Shouvik Islam <52196453+shouvik1712767642@users.noreply.github.com>
2020-08-18 10:02:00 +0600

    Shouvik1712767642
    part-2

---
+++ Part-2 Submission/1712767642_Q3.asm
@@ -0,0 +1,29 @@
+ .MODEL SMALL
+ .STACK 100H
+
+ .DATA
+    PROMPT  DB  
+
+ .CODE
+   MAIN PROC
+     MOV AX, @DATA                 
+     MOV DS, AX
+
+     LEA DX, PROMPT                
+     MOV AH, 9
+     INT 21H
+
+     MOV CX, 10                   
+     MOV AH, 2                    
+     MOV DL, 48                   
+
+     @LOOP:                       
+       INT 21H                    
+       INC DL                     
+       DEC CX                     
+       JNZ @LOOP                    
+
+     MOV AH, 4CH                  
+     INT 21H
+   MAIN ENDP
+ END MAIN
